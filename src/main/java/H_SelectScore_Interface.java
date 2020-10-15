11c7b32ae7df345a6d834a6b7d4353f5817c03e2
huihuijiangqiang <59404531+huihuijiangqiang@users.noreply.github.com>
2020-10-15 20:56:00 +0800

    -

---
+++ src/main/java/H_SelectScore_Interface.java
@@ -0,0 +1,4 @@
+public interface H_SelectScore_Interface {
+    public abstract String SelectScore_Name(String [][] str, String name);
+    public abstract String SelectScore_xh(String [][] str, String xh);
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,13 @@
 public class main {
     public static void main(String[] args) {
         W_ScoreManage_Interface w =new W_ScoreManage_Interface_IMPL();
+        H_SelectScore_Interface h=new H_SelectScore_Interface_IMPL();
         excel excel=new excel("D:\\github\\Task1014\\score.xlsx", "sheet2");
         String[][] str = excel.readExcelData();
         System.out.println(w.average_score(str,"物理成绩"));
         System.out.println(w.max_score(str,"物理成绩"));
+        System.out.println(h.SelectScore_Name(str,"费先彬1"));
+        System.out.println(h.SelectScore_xh(str,"100001.0"));
+
     }
 }
